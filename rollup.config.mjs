240221b9e1fa8d55e48dfe9e1afb7858973fffc6
Michio <mohsen@regentmarkets.com>
2023-11-04 19:06:59 +0800

    chore: separated icons types in separate import folder

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,33 +1,78 @@
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
-import terser from '@rollup/plugin-terser';
 import external from 'rollup-plugin-peer-deps-external';
+import { readdirSync } from 'fs';
 import dts from 'rollup-plugin-dts';
-import { readFileSync } from 'fs';
-const fileUrl = new URL('./package.json', import.meta.url);
-const packageJSON = JSON.parse(readFileSync(fileUrl));
+
+const getComponentsFolders = (entry) => {
+  const dirs = readdirSync(entry);
+  const dirsWithoutIndex = dirs.filter((name) => name !== 'index.ts');
+  return dirsWithoutIndex;
+};
+
+const commonPlugins = [
+  external(),
+  resolve(),
+  typescript({
+    tsconfig: './tsconfig.build.json',
+  }),
+];
+
+function component(commonPlugins, folder) {
+  return {
+    input: `src/react/${folder}/index.ts`,
+    output: [
+      {
+        file: `dist/${folder}/index.js`,
+        exports: 'named',
+        format: 'esm',
+      },
+    ],
+    plugins: [...commonPlugins],
+    external: [/node_modules/, 'react', 'react-dom', 'react/jsx-runtime'],
+  };
+}
+
+const componentDts = (folder) => {
+  return {
+    input: `dist/types/react/${folder}/index.d.ts`,
+    output: [{ file: `dist/${folder}/index.d.ts`, format: 'esm' }],
+    plugins: [
+      dts({
+        compilerOptions: {
+          baseUrl: './src',
+        },
+      }),
+    ],
+  };
+};
+
+const componentsFolders = getComponentsFolders('./src/react');
+
 /**
  * @type {import('rollup').RollupOptions[]}
  */
 export default [
   {
     input: 'src/index.ts',
-    output: [
-      {
-        file: packageJSON.main,
-        format: 'cjs',
-        sourcemap: true,
-      },
-      {
-        file: packageJSON.module,
-        format: 'esm',
-        sourcemap: true,
-      },
+    output: {
+      file: 'dist/index.js',
+      format: 'esm',
+    },
+    plugins: [
+      external(),
+      resolve(),
+      typescript({
+        tsconfig: './tsconfig.build.json',
+        compilerOptions: {
+          declaration: true,
+          declarationDir: './dist/types',
+        },
+      }),
     ],
-    plugins: [external(), resolve(), typescript({ tsconfig: './tsconfig.build.json' }), terser()],
   },
   {
-    input: 'dist/esm/types/index.d.ts',
+    input: 'dist/types/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
     plugins: [
       dts({
@@ -37,4 +82,6 @@ export default [
       }),
     ],
   },
+  ...componentsFolders.map((folder) => component(commonPlugins, folder)),
+  ...componentsFolders.map((folder) => componentDts(folder)),
 ];
