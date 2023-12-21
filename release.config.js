41944add3a0affa492841e66d7bb75ecfa9f42d3
Michio <mohsen@regentmarkets.com>
2023-12-21 11:13:15 +0800

    ci: updated release config, and release branch trigger name [skip ci]

--- release.config.js
+++ release.config.js
@@ -34,7 +34,7 @@ module.exports = {
           },
           {
             type: 'refactor',
-            release: 'patch',
+            release: 'minor',
           },
           {
             type: 'style',
