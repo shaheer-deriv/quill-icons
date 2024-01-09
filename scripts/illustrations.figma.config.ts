230686e149ce9caae56b71ed862712c239de9f30
Niloofar <niloofar.sadeghi@regentmarkets.com>
2024-01-09 11:04:28 +0800

    chore: this is a test commit

--- scripts/illustrations.figma.config.ts
+++ scripts/illustrations.figma.config.ts
@@ -9,7 +9,6 @@ import { IllustrationStoriesOptions } from './stories-configs/illustrations';
 
 dotenv.config();
 
-// need to change this file id
 const fileId: string = '4N66of3WfbhmHWkBiGvlT9';
 
 const outputters: ComponentOutputter[] = [
